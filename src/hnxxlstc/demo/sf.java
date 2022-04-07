92d27928064e3825e113d08e9bc1a04892568924
hnxxlstc <hnxxlstc@162.com>
2022-04-07 22:42:03 +0800

    Initial commit

---
+++ src/hnxxlstc/demo/sf.java
@@ -0,0 +1,12 @@
+package hnxxlstc.demo;
+
+/**
+ * @Author: hnxxlstc
+ * @Date: 2022-04-04 下午 11:22
+ */
+public class sf {
+    public static void main(String[] args) {
+        System.out.println("lixiaoyuxihuanshui");
+    }
+}
+
